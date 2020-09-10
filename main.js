825bc5be5b37abd44b391e2cadfa9d68bfd5f7ab
josephstapf <70560795+josephstapf@users.noreply.github.com>
2020-09-09 21:05:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Suck me, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Suck me, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Huh, ' + myName;
+  }
+}
